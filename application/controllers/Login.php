ad62a482075ed2bd6e8931e2be77af14bab3e6a5
rezkyrvl123 <86161952+rezkyrvl123@users.noreply.github.com>
2021-06-20 10:03:57 +0700

    empat

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,25 +12,29 @@ class Login extends CI_Controller {
 	{
 		$this->load->view('V_Login');
 	}
+    
 	function auth(){
         $username=htmlspecialchars($this->input->post('username',TRUE),ENT_QUOTES);
         $password=htmlspecialchars($this->input->post('password',TRUE),ENT_QUOTES);
  
         $cek_admin=$this->M_Login->auth_admin($username,$password);
-
-        if($cek_admin){
-			$data=$cek_admin->row_array();
-		
+        // $cek_penjual=$this->M_Login->auth_penjual($username,$password);
+        // $cek_pembeli=$this->M_Login->auth_pembeli($username,$password);
+ 
+        if($cek_admin->num_rows() > 0){
+            $data=$cek_admin->row_array();
+        
             $this->session->set_userdata('login',TRUE);
             $this->session->set_userdata('id',$data['id_admin']);
             $this->session->set_userdata('nama',$data['nama_admin']);
-			$this->session->set_userdata('hak_akses','Administrator');
-			
-			echo $this->session->set_flashdata('sukses','Login Sukses! Selamat Datang '+$data['nama_admin']+'!');
-            redirect('login');
-        }else{
-            echo $this->session->set_flashdata('gagal','Username Atau Password Salah');
-			redirect('Dashboard');
+            $this->session->set_userdata('hak_akses','Administrator');
+            
+            echo $this->session->set_flashdata('sukses','Login Sukses! Selamat Datang '+$data['nama_admin']+'!');
+            redirect('Dashboard');
         }
-	}
+        else{
+            echo $this->session->set_flashdata('gagal','Username Atau Password Salah');
+            redirect('Halaman_Utama');
+        }
+    }
 }
--- application/models/M_Login.php
+++ application/models/M_Login.php
@@ -4,7 +4,7 @@ class M_Login extends CI_Model
 {
     function auth_admin($username,$password)
 	{
-		$result = $this->db->query("select * from admin where username='$username' and password='$password' LIMIT 1");
+		$result=$this->db->query("select * from admin where username='$username' and password='$password' LIMIT 1");
 		return $result;
 	}
 }
